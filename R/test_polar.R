84cd7f3d8c681f9d1c76b05298f0a2a6b86eedbb
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-10-04 20:02:31 -0500

    Added constraints-- clumsy, because we basically convert to cartesian each time for the sole purpose of filtering out the out-of-range values, but at least it works. The new sample_polar() function should do everything needed right up to the simulation step which is the next function to write. The center parameter doesn't seem to work for some reason. Next step: make a proper simulation function and test-panel function.

--- R/test_polar.R
+++ R/test_polar.R
@@ -46,42 +46,81 @@ crt2pol <- function(xx,center=kmeans(xx,1)$centers,sort=c('angle','radius','none
 
 ## convert polar coordinates back to cartesian
 ## returns a matrix of same dimension
-pol2crt <- function(pp,center=if('center'%in%names(attributes(pp))) as.numeric(attr(pp,'center')) else rep(0,len=ncol(pp))){
+pol2crt <- function(pp
+                    ,center=if('center'%in%names(attributes(pp))) as.numeric(attr(pp,'center')) else rep(0,len=ncol(pp))
+                    ,maxes=Inf,mins=-Inf,denom=2
+                    ){
   ## pp = polar coordinate matrix [r,theta]
   ## center = point on which the cartesian result should be centered
-  oo<-scale(cbind(x=pp[,1]*cos(pp[,2]),y=pp[,1]*sin(pp[,2])),center= -center,scale=F);attr(oo,'center')<-center;oo;
+  oo<-scale(cbind(x=pp[,1]*cos(pp[,2]),y=pp[,1]*sin(pp[,2])),center= -center,scale=F);attr(oo,'center')<-center;
+  # if(!isTRUE(abs(maxes)==Inf)&!isTRUE(abs(mins)==Inf)){
+  #   # find the out-of-range values
+  #   oor<-apply(oo,1,function(xx) any(xx>maxes)||any(xx<mins));
+  #   oo[oor,]<-oo[sample(which(!oor),sum(oor),rep=T),];
+  #   cat('\nReplacing ',sum(oor),' out-of-range values via resample')
+  #   rngs <- (maxes - mins);
+  #   cat('\nScaling...\n');
+  #   oo <- t(apply(oo,1,function(xx) (xx-mins)/rngs));
+  # }
+  oo;
+}
+
+sample_polar <- function(nn=1000,rmax=3,thetawrap=0.5
+                         ,ntheta=1,center=c(0,0),maxes,mins){
+  oo <- cbind(r=runif(nn,0,rmax),theta=runif(nn,0,(2+thetawrap)*pi));
+  oocr <- pol2crt(oo,center=center);
+  if(!missing(maxes)&!missing(mins)){
+    oor <- apply(oocr,1,function(xx) any(xx>maxes)||any(xx<mins));
+    oo <- oo[!oor,]; oocr <- oocr[!oor,];
+  }
+  return(list(spol=oo,scrt=oocr));
 }
 
 #' ### Here we try it
 #' 
+maxes <- c(1.5,1.8); mins<-c(-1.65,-2.3);
+ctr <- c(0.3,-0.2); # okay, so the center parameter works...
 #' Sample from a polar space immediately converting to cartesian
-spol<-cbind(r=runif(1000,0,4),theta=runif(1000,0,2.5*pi));
+#' TODO: create a sample_polar function
+spol<-sample_polar(rmax=4,maxes=maxes,mins=mins,center=ctr)$spol;
+#spol<-cbind(r=runif(1000,0,3),theta=runif(1000,0,2.5*pi));
 #' generate binomial outcomes (we are going to look for the 0.8 contour)
-spol<-cbind(spol,res=apply(pol2crt(spol),1,function(zz) gen_binom(zz[1],zz[2])));
+spol<-cbind(spol,res=apply(pol2crt(spol,center = ctr),1,function(zz) gen_binom(zz[1],zz[2])));
 #' Fit a model
-prmod <- glm(res~r*sin(theta),data.frame(spol),family='binomial');
+prmod <- glm(res~r*sin(theta)*cos(theta),data.frame(spol),family='binomial');
 #' The following parts get repeated many times
-spol0 <- cbind(r=runif(1000,0,4),theta=runif(1000,0,2.5*pi));
-spol0 <- spol0[
-  with(predict(
-    update(prmod,data=data.frame(spol))
-    ,data.frame(spol0),type='response',se.fit=T)
-    ,{
-      prs <- abs(fit-0.2)^3/se.fit;
-      #prs <- abs(fit-0.2);
-      which(prs<quantile(prs,.01));
-      }),];
-spol <- rbind(spol,cbind(spol0,res=apply(pol2crt(spol0),1,function(zz) gen_binom(zz[1],zz[2]))));
+newsmp <- sample_polar(rmax=4,maxes=maxes,mins=mins,center=ctr);
+#spol0 <- cbind(r=runif(1000,0,3),theta=runif(1000,0,2.5*pi));
+#scrt0 <- pol2crt(spol0,center=ctr);
+#oor <- apply(scrt0,1,function(xx) any(xx>maxes)||any(xx<mins));
+#scrt0 <- scrt0[!oor,]; spol0 <- spol0[!oor,];
+bestfit <- with(predict(
+  update(prmod,data=data.frame(spol))
+  ,data.frame(newsmp$spol),type='response',se.fit=T)
+  ,{prs <- abs(fit-0.2)^3/se.fit;
+    #prs <- abs(fit-0.2);
+    which(prs<quantile(prs,.05));});
+newsmp <- sapply(newsmp,function(xx) xx[bestfit,],simplify=F);
+#spol <- spol[bestfit,]; scrt0 <- scrt0[bestfit,];
+spol <- rbind(spol,cbind(newsmp$spol,res=apply(newsmp$scrt,1,function(zz) gen_binom(zz[1],zz[2]))));
 #' End repeat/update part
 #' 
 #' Below are the visualizations that can be done on any iteration
-plot(spol[,-3],pch='.',col='#00000030',xlim=c(1.5,2.5));
-points(spol0,pch='.',col='red');
+plot(spol[,-3],pch='.',col='#00000050'); #,xlim=c(1.5,2.5));
+points(newsmp$spol,pch='.',col='red');
 #' How good is the fitted contour?
 bar <-abs(predict(update(prmod),type='response')-0.2);
-foo<-pol2crt(spol[which(bar<quantile(bar,.1)),-3]);plot(foo,pch='.',col='#00000099');dim(foo);
+foo<-pol2crt(spol[which(bar<0.01),-3,drop=F],center=ctr);plot(foo,pch='.',col='#00000099');dim(foo);
 #' Run the following once only after the first few iteration, for reference
 # bar.bak <- bar; foo.bak <- foo;
 points(foo.bak,col='red',pch='+');
 #' How does this look on polar coordinates?
-plot(spol[which(bar.bak<quantile(bar.bak,.1)),-3],col='red');points(spol[which(bar<quantile(bar,.1)),-3]);
+plot(spol[which(bar.bak<quantile(bar.bak,.1)),-3],col='red',xlim=range(spol[,1]),ylim=range(spol[,2]));
+points(spol[which(bar<quantile(bar,.1)),-3]);
+#' Note: if the distribution is not logistic, so far it's in a way
+#' that does not cause it to be zero-inflated, overdispersed, or 
+#' heteroscedastic
+#' 
+#' Let's see if this holds up when we are using real p-values.
+#' But first we need to get bounds and centering to work.
+#' 
