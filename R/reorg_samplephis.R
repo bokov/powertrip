e3bd62bbf12f50df5ca3c6ca1c32df181b72ba0b
Alex F. Bokov <bokov@uthscsa.edu>
2017-12-21 08:37:53 -0500

    Trying to further diminish the failure rate by detecting the apparently common failure due to too many detections and trying again with a min radius of 0 instead of failing

--- R/reorg_samplephis.R
+++ R/reorg_samplephis.R
@@ -469,7 +469,10 @@ phi_radius <- function(phi,maxrad,pnlst,pnlph,refcoords
     preds <- try(sapply(testtf,resp_preds,radii=testrd));
     if(class(preds)[1]=='try-error') browser();
     #preds<-sapply(na.omit(data.frame(do.call(rbind,list_tfresp))),resp_preds,radii=na.omit(unlist(list_radii)));
-    lims <- preds_lims(preds,limit=maxrad,numse = numse);
+    new.lims <- preds_lims(preds,limit=maxrad,numse = numse);
+    # if the reason for failing is simply that it's too easy to detect a difference
+    # lower the min lim and try again
+    if(new.lims['status']==-1 && mean(testtf,na.rm=T)>.9) lims['min']<-0 else lims <- new.lims;
     #if(any(na.omit(lims[c('min','max')])<0|na.omit(lims[c('min','max')])>maxrad)) browser(text='Invalid limits!');
     # Currently, we give up on this entire set of phis and exit from phi_radius()
     # the first cycle when glm fails for all panel functions regardless of why.
