737c9d20e054a68065a215d8943c83d825462fab
Alex F. Bokov <bokov@uthscsa.edu>
2018-01-08 17:25:07 -0600

    Added misc directory for non-package files that are starting to need tracking

---
+++ misc/runexample.R
@@ -0,0 +1,9 @@
+options(error=recover);
+library(Survomatic)
+logenv <- load.ptenv('pt_result.rdata')
+pnlst_gmcx<-logenv$state$powertrip$pnlst
+lrefcoords<-log(refcoords<-c(3.02495622562167e-06,0.00766970877053115,1.97042457165941e-05));
+lrelmaxs<-((lmaxs<-log(maxs <- c(1e-04, 0.0195827842383701, 0.001145822)))-lrefcoords);
+lrelmins<-((lmins<-log(mins <- c(2.29371429246515e-08, 0.00445842550313631, 1e-16)))-lrefcoords);
+.out <- powertrip(logenv,refcoords=lrefcoords,maxs=lrelmaxs,mins=lrelmins,npoints=100,pnlst=pnlst_gmcx,ptsim=ptsim_surv,nrads=60,backtrans=exp,type='gm',tol=0.1)
+#savehistory(file='runscrap.R')
