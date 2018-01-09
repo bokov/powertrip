bf3dc4512c92cb5b587d8ec2c898e6d48bb05c61
Alex F. Bokov <bokov@uthscsa.edu>
2018-01-08 22:21:17 -0600

    Typo in maxs in runexample.R

--- misc/runexample.R
+++ misc/runexample.R
@@ -11,7 +11,7 @@ if(file.exists(logenv_file)){
 lrefcoords<-log(refcoords<-c(3.02495622562167e-06,0.00766970877053115,1.97042457165941e-05));
 # lrelmaxs<-((lmaxs<-log(maxs <- c(1e-04, 0.0195827842383701, 0.001145822)))-lrefcoords);
 # phicycle 142: expanding maxs to... 
-lrelmaxs<-((lmaxs<-log(maxs <- c(0.000448943309591518, 0.0195827842383701, 0.058737528647514);
+lrelmaxs<-((lmaxs<-log(maxs <- c(0.000448943309591518, 0.0195827842383701, 0.058737528647514)))-lrefcoords);
 # lrelmins<-((lmins<-log(mins <- c(2.29371429246515e-08, 0.00445842550313631, 1e-16)))-lrefcoords);
 # phicycle 142: expanding to mins to... 
 lrelmins<-((lmins<-log(mins <- c(1.01475976473711e-09, 0.00103798220880217, 1e-16)))-lrefcoords);
