90337f44ccd3ba8ae15985be70d9567221d83f5a
Alex F. Bokov <bokov@uthscsa.edu>
2018-01-19 15:26:54 -0600

    Oops, need to uncomment the reading of the previous logenv file in runexample.R.

--- misc/runexample.R
+++ misc/runexample.R
@@ -1,14 +1,14 @@
 options(error=recover);
 library(Survomatic);
 logenv_file <- 'pt_result.rdata';
-# if(file.exists(logenv_file)){
-#   logenv <- load.ptenv(logenv_file,savewait = 0);
-#   pnlst_gmcx<-logenv$state$powertrip$pnlst;
-# } else {
-#   logenv<-new.env();
-#   pnlst_gmcx <- list(cx=ptpnl_sr,gm=ptpnl_gm,sims=ptpnl_simsumm);
-# }
-tol<-0.1;
+if(file.exists(logenv_file)){
+   logenv <- load.ptenv(logenv_file,savewait = 0);
+   #pnlst_gmcx<-logenv$state$powertrip$pnlst;
+ } else {
+   logenv<-new.env();
+   #pnlst_gmcx <- list(cx=ptpnl_sr,gm=ptpnl_gm,sims=ptpnl_simsumm);
+ }
+#tol<-0.1;
 lrefcoords<-log(refcoords<-c(3.02495622562167e-06,0.00766970877053115,1.97042457165941e-05));
 # lrelmaxs<-((lmaxs<-log(maxs <- c(1e-04, 0.0195827842383701, 0.001145822)))-lrefcoords);
 # phicycle 142: expanding maxs to... 
